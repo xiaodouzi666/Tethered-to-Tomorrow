877fe34631272f9e9dfb98b1bc149fb5eb212bf6
xiaodouzi666 <j.liu.86@student.rug.nl>
2026-05-19 01:59:31 +0200

    Ignore generated local files

--- frontend/node_modules/openmct/src/plugins/inspectorViews/annotations/tags/TagEditorClassNames.js
+++
@@ -1,9 +0,0 @@
-const TagEditorClassNames = Object.freeze({
-  REMOVE_TAG: 'js-remove-tag',
-  AUTOCOMPLETE_INPUT: 'js-autocomplete__input',
-  ADD_TAG_BUTTON: 'js-add-tag-button',
-  ADD_TAG_LABEL: 'js-add-tag-label',
-  TAG_OPTION: 'js-tag-option'
-});
-
-export default TagEditorClassNames;
--- frontend/node_modules/openmct/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const SupportedViewTypes = [
-  'plot-stacked',
-  'plot-overlay',
-  'bar-graph.view',
-  'time-strip.view',
-  'example.imagery',
-  'timelist.view'
-];
